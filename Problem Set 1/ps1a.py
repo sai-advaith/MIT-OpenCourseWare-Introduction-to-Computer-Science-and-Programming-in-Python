5d02b7553501c658bc5abbc901474f19b7650aca
Advaith Sai <advaith.maddipatla@gmail.com>
2019-11-20 00:06:11 +0000

    PS1

---
+++ Problem Set 1/ps1a.py
@@ -0,0 +1,7 @@
+annual_salary = float (input ("Annual Salary:"))
+portion_saved = float (input ("Portion of Salary Saved: "))
+total_cost = float (input ("Cost of dream home: "))
+portion_down_payment = 0.25
+current_savings = 0
+r = 0.04
+# defining all the variables that will be used in this given program
