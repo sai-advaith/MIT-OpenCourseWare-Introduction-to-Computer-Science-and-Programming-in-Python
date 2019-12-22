b7e289d3b47e444b5023d32f65ad63d5c6d7505d
Advaith Sai <advaith.maddipatla@gmail.com>
2019-12-22 18:18:00 +0400

    complete ps1c

---
+++ Problem Set 1/ps1c.py
@@ -0,0 +1,13 @@
+#   variables to be inputted 
+
+annual_salary = float (input ("Enter your Annual Salary:"))
+portion_saved = float (input ("Enter the percent of your salary to save, as a decimal:"))
+total_cost = 1000000
+semi_annual_raise = 0.07
+portion_down_payment = 0.25*total_cost
+r = 0.04
+lower = 0
+upper = 10000
+delta = 100
+current_savings = 0
+month = 0
